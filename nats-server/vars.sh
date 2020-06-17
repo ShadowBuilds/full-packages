40659546c42143dfe90d364aab1dc20c15453c69
Phil Pennock <phil+git@pennock-tech.com>
2020-06-17 01:14:23 -0400

    systemd configurations written

--- nats-server/vars.sh
+++ nats-server/vars.sh
@@ -1,2 +1,3 @@
 : "${SERVER_REPO:=https://github.com/nats-io/nats-server}"
 : "${GIMME_GO_VERSION=1.13.x}"
+: "${NEED_USER:=nats}"
--- nats-streaming-server/vars.sh
+++ nats-streaming-server/vars.sh
@@ -1,2 +1,3 @@
 : "${SERVER_REPO:=https://github.com/nats-io/nats-streaming-server}"
 : "${GIMME_GO_VERSION=1.13.x}"
+: "${NEED_USER:=nats}"
